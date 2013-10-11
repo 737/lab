1b5631ebe0a1e28bedb45f444fbc6678f957cfa3
unknown <Alex.Li@T03906--216.augmentum.com.cn>
2013-10-11 10:50:32 +0800

    aug alex

--- sunjs/sun.util.js
+++ sunjs/sun.util.js
@@ -146,3 +146,37 @@ sun.util.stringFormat = function(txt) {
 
     return str;
 };
+
+/**
+ * 
+ * eg. format = 'yyyy-MM-dd hh:mm:ss'
+ * 
+ */
+sun.util.getCurrentTime = function(format) {
+    var _this = new Date();
+    var o = {
+        "M+": _this.getMonth() + 1, //month
+        "d+": _this.getDate(), //day
+        "h+": _this.getHours(), //hour
+        "m+": _this.getMinutes(), //minute
+        "s+": _this.getSeconds(), //second
+        "q+": Math.floor((_this.getMonth() + 3) / 3), //quarter
+        "S": _this.getMilliseconds() //millisecond
+    }
+
+    if(!format) {
+        format = "yyyy-MM-dd hh:mm:ss";
+    }
+
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1, (_this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(format))
+            format = format.replace(RegExp.$1,
+                RegExp.$1.length == 1 ? o[k] :
+                ("00" + o[k]).substr(("" + o[k]).length));
+    return format;
+};
+
+sun.util.isEven = function(num) {
+    return num % 2 == 0 ? true : false;
+};
