88eb84eb04e3e2c6de7979173676e976543edb7b
Arleigh.Li <suncms@qq.com>
2014-12-30 00:18:24 +0800

    add the grunt function

---
+++ sun/gruntfile.js
@@ -0,0 +1,20 @@
+﻿module.exports = function (grunt) {
+    
+    grunt.initConfig({
+        pkg : grunt.file.readJSON('package.json'),
+        uglify : {
+            options : {
+                banner : '/* <%= pkg.file %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            my_tartget: {
+                files: {
+                    'dest/<%= pkg.file%>.min.js': ['src/*.js']
+                }
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
+    grunt.registerTask('default', ['uglify']);
+}
