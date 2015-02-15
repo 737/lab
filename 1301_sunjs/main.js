90fd310cc4e9c06a42c63b4b3dc7fbc2309c1044
liwl <liwl@Ctrip.com>
2015-02-15 16:11:48 +0800

    update

--- 1301_sunjs/main.js
+++ 1301_sunjs/main.js
@@ -12,29 +12,6 @@ require.config({
 
 require(['jQuery', 'underscore', 'sun'], function () {
     
-    
-        
-    var srcElm = $('#searchBox');
-    var elmBlur = $('#opciones');
-    
-    srcElm.click(function (evt) {
-        elmBlur.slideDown();
-        
-        sun.autoBlur(srcElm[0], elmBlur[0], function () {
-            elmBlur.hide();
-        });
-    });
-    
-    var srcElm2 = $('#searchBox2');
-    var elmBox2 = $('#box2');
-    
-    srcElm2.click(function (evt){
-        elmBox2.show();
-        
-        sun.autoBlur(srcElm2[0], elmBox2[0], function () {
-            elmBox2.hide();
-        });
-    });
 });
 
 function getTemplateUrl (sUrl) {
