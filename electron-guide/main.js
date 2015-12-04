69a5d85ad57d5497b1acf4fffca79a75f56cf858
liwl <liwl@Ctrip.com>
2015-12-04 16:02:00 +0800

    electron

--- electron-guide/main.js
+++ electron-guide/main.js
@@ -5,55 +5,11 @@ var BrowserWindow = require('browser-window');
 
 var mainWindow = null;
 
-var globalShortcut = require('global-shortcut');
-
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
-        frame: false,
-        height: 700,
-        resizable: false,
-        width: 368
+        height: 600,
+        width: 800
     });
 
     mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
-
-    globalShortcut.register('ctrl+shift+1', function () {
-        mainWindow.webContents.send('global-shortcut', 0);
-    });
-    globalShortcut.register('ctrl+shift+2', function () {
-        mainWindow.webContents.send('global-shortcut', 1);
-    });
-});
-
-var ipc = require('ipc');
-
-ipc.on('close-main-window', function () {
-    app.quit();
-});
-
-var settingsWindow = null;
-
-ipc.on('open-settings-window', function () {
-    if (settingsWindow) {
-        return;
-    }
-
-    settingsWindow = new BrowserWindow({
-        frame: false,
-        height: 200,
-        resizable: false,
-        width: 200
-    });
-
-    settingsWindow.loadUrl('file://' + __dirname + '/app/settings.html');
-
-    settingsWindow.on('closed', function () {
-        settingsWindow = null;
-    });
-});
-
-ipc.on('close-settings-window', function () {
-    if (settingsWindow) {
-        settingsWindow.close();
-    }
 });
--- electron-guide/readme.md
+++ electron-guide/readme.md
@@ -1,8 +1,3 @@
 # Accompanying repository for the Electron guide
 
 ![Sound Machine](https://rawgithub.com/bojzi/sound-machine/master/sketch/sound-machine.png)
-
-
-
-// 参考
-http://www.liuhaihua.cn/archives/124970.html
